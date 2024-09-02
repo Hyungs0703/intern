19741009118e7c664aa2d8684bbdcf401770b140
Hyungs0703 <hyungs0703@naver.com>
2024-09-02 17:14:16 +0900

    csrf 비활성화

--- src/main/java/com/example/intern/global/config/WebSecurityConfig.java
+++ src/main/java/com/example/intern/global/config/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -37,10 +38,14 @@ public class WebSecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
 		http
+			.csrf(AbstractHttpConfigurer::disable)
+
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
 			.authorizeHttpRequests(authorize -> authorize
-				.requestMatchers(HttpMethod.POST, "/api/**").permitAll())
+				.requestMatchers(HttpMethod.POST, "/api/**").permitAll()
+				.anyRequest().authenticated())
+
 
 			.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 
