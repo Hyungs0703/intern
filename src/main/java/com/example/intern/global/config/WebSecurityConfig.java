7a9dcbe27a1ebe75efc28d0718301495f566a812
Hyungs0703 <hyungs0703@naver.com>
2024-09-02 16:06:06 +0900

    JwtAuthorizationFilter 필터 추가

--- src/main/java/com/example/intern/global/config/WebSecurityConfig.java
+++ src/main/java/com/example/intern/global/config/WebSecurityConfig.java
@@ -1,19 +1,37 @@
 package com.example.intern.global.config;
 
+import com.example.intern.jwt.JwtUtil;
+import com.example.intern.jwt.security.JwtAuthorizationFilter;
+import com.example.intern.jwt.security.UserDetailServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
+	private final JwtUtil jwtUtil;
+	private final UserDetailServiceImpl userDetailService;
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
+	}
+
+	@Bean
+	public JwtAuthorizationFilter jwtAuthorizationFilter() {
+		return new JwtAuthorizationFilter(jwtUtil, userDetailService);
+	}
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -22,7 +40,9 @@ public class WebSecurityConfig {
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
 			.authorizeHttpRequests(authorize -> authorize
-				.requestMatchers(HttpMethod.POST, "/api/**").permitAll());
+				.requestMatchers(HttpMethod.POST, "/api/**").permitAll())
+
+			.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
 	}
